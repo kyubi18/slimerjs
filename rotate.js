42654df01e2547fcfc478eb0926b041ac12c38fe
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-04-02 19:29:57 +0700

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,4 +1,4 @@
-		const directCostom="off";//on/off
-		const directRandom="no";//yes/np
-		var linkDirectRandom=["https://google.com","https://www.youtube.com"];
-		var linkDirectOne=linkDirectRandom[random(0, (linkDirectRandom.length-1))];
+const directCostom="off";//on/off
+const directRandom="no";//yes/np
+var linkDirectRandom=["https://google.com","https://www.youtube.com"];
+var linkDirectOne=linkDirectRandom[random(0, (linkDirectRandom.length-1))];
