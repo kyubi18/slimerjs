f127aa00fdc2d58aac8c0b721ff5a8f2e95ba2d4
kyubi18 <66015930+kyubi18@users.noreply.github.com>
2021-04-02 19:36:50 +0700

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,4 +1,8 @@
+//--- costom direct ---
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 const directCostom="on";//on/off
-const directRandom="yes";//yes/np
+const directRandom="yes";//yes/no
 var linkDirectRandom=["https://google.com","https://www.youtube.com"];
 var linkDirectOne=linkDirectRandom[random(0, (linkDirectRandom.length-1))];
